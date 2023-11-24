38c6f480a11c9807c084acc48c07c80b3b3a67ed
Алексей Никитин <a.nikitin@k-telecom.org>
2023-11-24 17:00:42 +0500

    fix

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,6 +2,6 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dash') }}">Очередь производства</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('equipment') }}">Мониторинг оборудования</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('equipment') }}">Оборудование</a></li>
+<!-- <li class="nav-item"><a class="nav-link" href="{{ backpack_url('equipment') }}">Оборудование</a></li> -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('queue') }}">Технологические карты</a></li>
 <!-- <li class="nav-item"><a class="nav-link" href="{{ backpack_url('operation') }}">Операции</a></li> -->
