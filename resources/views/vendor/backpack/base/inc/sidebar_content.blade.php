2dc1a6ad0dc7cab9cf92b387d6bb9905f965a11a
Алексей Никитин <a.nikitin@k-telecom.org>
2023-11-25 12:47:17 +0500

    fix

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,8 +1,8 @@
 {{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dash') }}">Очередь производства</a></li>
-
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('work') }}">План участка работ</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('equipment') }}">Мониторинг оборудования</a></li>
 <!-- <li class="nav-item"><a class="nav-link" href="{{ backpack_url('equipment') }}">Оборудование</a></li> -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('queue') }}">Технологические карты</a></li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('work') }}">План участка работ</a></li>
+
 <!-- <li class="nav-item"><a class="nav-link" href="{{ backpack_url('operation') }}">Операции</a></li> -->
